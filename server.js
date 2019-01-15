281f74cf27258397bfd2e0a896971c4e68add8be
SimonDevelop <contact@simon-micheneau.fr>
2019-01-15 11:59:33 +0100

    test: test access repos in config.json

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const config = require("./config.json");
 const request = require('request');
 const apiUrl = "https://api.github.com";
 
-var options = {
-    url: apiUrl+"/repos/SimonDevelop/releaser-bot-test",
-    headers: {
-        'User-Agent': 'releaser-bot',
-        'Authorization': 'token '+config.token
+for (var i=0; i < config.repositories.length; i++) {
+    var options = {
+        url: apiUrl+"/repos/"+config.repositories[i],
+        headers: {
+            'User-Agent': 'releaser-bot',
+            'Authorization': 'token '+config.token
+        }
+    };
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body);
+            console.log(info);
+        }
     }
-};
-
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body);
-        console.log(info);
-    }
+    request(options, callback);
 }
-
-request(options, callback);
