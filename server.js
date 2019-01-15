eced239fd1e8bc6da30f56b7d847f5e5955f4f6e
SimonDevelop <contact@simon-micheneau.fr>
2019-01-15 14:55:15 +0100

    test: test steps request with events response

--- server.js
+++ server.js
@@ -1,20 +1,49 @@
 const config = require("./config.json");
 const request = require('request');
-const apiUrl = "https://api.github.com";
+var EventEmitter = require("events").EventEmitter;
+var data = new EventEmitter();
+var apiUrl = "https://api.github.com";
 
 for (var i=0; i < config.repositories.length; i++) {
+    // Default options
     var options = {
         url: apiUrl+"/repos/"+config.repositories[i],
         headers: {
+            'Content-type': 'application/json',
             'User-Agent': 'releaser-bot',
             'Authorization': 'token '+config.token
-        }
+        }, json: true
     };
-    function callback(error, response, body) {
+
+    // Check repos
+    request(options, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            var info = JSON.parse(body);
-            console.log(info);
+            data.json = body;
+            data.emit('update');
         }
-    }
-    request(options, callback);
+    });
 }
+
+// Check last release
+data.on('update', function () {
+    options.url = apiUrl+"/repos/"+data.json.full_name+"/releases"
+
+    // Charge last release
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            data.releases = body;
+            data.emit('release');
+        }
+    });
+});
+
+data.on('release', function () {
+    console.log(data.releases);
+    // if (releases.length > 0) {
+    //     lastRelease.tag = releases[0].tag_name;
+    //     lastRelease.name = releases[0].name;
+    // } else {
+    //     lastRelease.tag = "0.0.1";
+    //     lastRelease.name = "Release 0.0.1";
+    // }
+});
