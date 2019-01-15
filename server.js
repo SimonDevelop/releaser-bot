8bb5e63bfb798cf950ae95d88daa8e85d49ad05a
SimonDevelop <contact@simon-micheneau.fr>
2019-01-15 11:13:34 +0100

    test: test access to api github without api key

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
-const config = require("./config.json");
+// const config = require("./config.json");
 const request = require('request');
+const apiUrl = "https://api.github.com";
+
+var options = {
+    url: apiUrl,
+    headers: {
+        'User-Agent': 'releaser-bot'
+    }
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var info = JSON.parse(body);
+        console.log(info);
+    }
+}
+
+request(options, callback);
