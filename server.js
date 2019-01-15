b3443d79afa524e25844c4db07c1e6c03c1c4783
SimonDevelop <contact@simon-micheneau.fr>
2019-01-15 11:55:12 +0100

    test: test access with api token

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-// const config = require("./config.json");
+const config = require("./config.json");
 const request = require('request');
 const apiUrl = "https://api.github.com";
 
 var options = {
-    url: apiUrl,
+    url: apiUrl+"/repos/SimonDevelop/releaser-bot-test",
     headers: {
-        'User-Agent': 'releaser-bot'
+        'User-Agent': 'releaser-bot',
+        'Authorization': 'token '+config.token
     }
 };
 
